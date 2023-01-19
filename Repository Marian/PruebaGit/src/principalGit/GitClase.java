995e894788b72eb9583ac51b27d4613397f93646
mdegs02@educastur.es <120382421+RitaCantaora@users.noreply.github.com>
2023-01-19 12:10:08 +0100

    Add files via upload

---
+++ Repository Marian/PruebaGit/src/principalGit/GitClase.java
@@ -0,0 +1,14 @@
+package principalGit;
+
+public class GitClase {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("hola mundo");
+		String a = "version prueba github ";
+		System.out.println("Adios Mundo Cruel");
+
+	}
+
+}
